4d116ffba4e13fbb2adeab96fcc64e2ab59d2c04
Nicolas Jacquemin <nicolas.jacquemin@photobox.com>
2020-04-28 19:01:18 +0200

    Reload order after status update

--- views/js/maborderconfirm.js
+++ views/js/maborderconfirm.js
@@ -1,28 +1,26 @@
 /*
  * 2020 Mon Atelier Bronzage
  */
+function maborderconfirm(form) {
+  var action = $('#markAsReceived').attr("action");
+  var paramString = "ajax=true";
+  $(form).find('input').each(function () {
+    if ($(this).attr('type') !== "submit") {
+      paramString += '&' + $(this).attr('name') + '=' + encodeURIComponent($(this).val());
+    }
+  });
+  
+  var orderId = $(form).find("input[name=orderId]")[0].value;
+  var file = action + '&id_order=' + orderId;
 
-function markAsReceived()
-{
-	paramString = "ajax=true";
-	$('#markAsReceived').find('input').each(function(){
-		paramString += '&' + $(this).attr('name') + '=' + encodeURIComponent($(this).val());
-	});
-	$.ajax({
-		type: "POST",
-		headers: { "cache-control": "no-cache" },
-		url: $('#markAsReceived').attr("action") + '?rand=' + new Date().getTime(),
-		data: paramString,
-		success: function (msg){
-          // TODO dynamic block-order-detail ID
-			$('#block-order-detail').fadeOut('slow', function() {
-				$(this).html(msg);
-				//catch the submit event of sendOrderMessage form
-				
-				$(this).find('#markAsReceivedBtn').fadeOut;
-				$(this).fadeIn('slow');
-			});
-		}
-	});
-	return false;
+  $.ajax({
+    type: "POST",
+    headers: {"cache-control": "no-cache"},
+    url: action + '&rand=' + new Date().getTime(),
+    data: paramString,
+    success: function () {
+      showOrder(1, orderId, file);
+    }
+  });
+  return false;
 }
--- views/templates/maborderconfirm.tpl
+++ views/templates/maborderconfirm.tpl
@@ -1,7 +1,7 @@
 {if $order_history.0.id_order_state == 4}
-  <form action="{$link->getPageLink('order-detail', true)|escape:'html'}" method="post" class="std" id="markAsReceived">
-    <input type="hidden" class="hidden" value="{$order->id|intval}" name="id_order" />
-    <input type="submit" class="btn btn-default" name="markAsReceived" id="markAsReceivedBtn" value="{l s='I have received this order' mod='maborderconfirm'}">
-    <p class="clear"></p>
+  <form action="{$link->getPageLink('order-detail', true)|escape:'html'}" onsubmit="maborderconfirm(this);return false;" method="post" class="std" id="markAsReceived">
+    <input type="hidden" class="hidden" value="{$order->id|intval}" name="orderId" />
+    <input type="hidden" class="hidden" value="{$order_history.0.id_order_state}" name="markAsReceived" />
+    <input type="submit" class="btn btn-default" value="{l s='I have received this order' mod='maborderconfirm'}">
   </form>
 {/if}
